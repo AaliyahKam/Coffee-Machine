fbeb1a0f010c6fa50a4488bb654f1bd68c4ca428
AaliyahKam <111664012+AaliyahKam@users.noreply.github.com>
2022-10-11 21:03:43 -0400

    Add files via upload

---
+++ CoffeeMachine.cpp
@@ -0,0 +1,57 @@
+ 1 def CoffeeMachhine
+ 2
+ 3 quantity = 1
+ 4 price = 700
+ 5
+ 6 if 1 <= quantity <= 4:
+ 7     price = quantity * 700
+ 8
+ 9 elif 5 <= quantity <= 8:
+10     price = quantity * 650
+11
+12 elif quantity > 8:
+13     price = quantity * 600
+14
+15 print(f"This order has a quantity of {quantity} coffee machines, with a total price of ${price}.")
+16
+17 quantity = 2
+18 price = 700
+19
+20 if 1 <= quantity <= 4:
+21     price = quantity * 700
+22
+23 elif 5 <= quantity <= 8:
+24     price = quantity * 650
+25
+26 elif quantity > 8:
+27     price = quantity * 600
+28
+29 print(f"This order has a quantity of {quantity} coffee machines, with a total price of ${price}.")
+30
+31 quantity = 8
+32 price = 700
+33
+34 if 1 <= quantity <= 4:
+35     price = quantity * 700
+36
+37 elif 5 <= quantity <= 8:
+38     price = quantity * 650
+39
+40 elif quantity > 8:
+41     price = quantity * 600
+42
+43 print(f"This order has a quantity of {quantity} coffee machines, with a total price of ${price}.")
+44
+45 quantity = 9
+46 price = 700
+47
+48 if 1 <= quantity <= 4:
+49     price = quantity * 700
+50
+51 elif 5 <= quantity <= 8:
+52     price = quantity * 650
+53
+54 elif quantity > 8:
+55     price = quantity * 600
+56
+57 print(f"This order has a quantity of {quantity} coffee machines, with a total price of ${price}.")
